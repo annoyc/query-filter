a40c852e6648b411c2361f0b0a191019f36bbbcd
kevinawe <yc2hangycz@gmail.com>
2022-05-28 21:36:10 +0800

    update README

--- examples/src/main.js
+++ examples/src/main.js
@@ -1,11 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
-import Antd from "ant-design-vue";
-import "ant-design-vue/dist/antd.css";
+// import Antd from "ant-design-vue";
+// import "ant-design-vue/dist/antd.css";
 import "@annoyc/query-filter/dist/style.css";
 import queryFilter from '@annoyc/query-filter';
 
-Vue.use(Antd);
+// Vue.use(Antd);
 Vue.use(queryFilter)
 
 Vue.config.productionTip = false;
